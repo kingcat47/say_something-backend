e25eedc482339ba72f9531511f6ea3f71cf0311c
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-12 16:23:01 +0900

    asdfasdfasdfasdfasdf

--- src/text/text.module.ts
+++ src/text/text.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TextEntity } from './entities/text.entity';
+import { Port_TextEntity } from './entities/port_text.entity';
 import { TextController } from './text.controller';
 import { TextService } from './text.service';
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([TextEntity, Port_TextEntity]),
+  ],
   controllers: [TextController],
-  providers: [TextService]
+  providers: [TextService],
 })
 export class TextModule {}
