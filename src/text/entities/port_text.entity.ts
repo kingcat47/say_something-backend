dd945d5e1607f63bd7ff30ad15ab6cf08d1f569a
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-12 16:27:07 +0900

    asdfasdfasdfasdfasdf

--- src/text/entities/port_text.entity.ts
+++ src/text/entities/port_text.entity.ts
@@ -3,7 +3,7 @@ import { Entity, PrimaryColumn, Column } from 'typeorm';
 @Entity('port_text')
 export class Port_TextEntity {
     @PrimaryColumn({ type: 'varchar', length: 50 })
-    port_str: string;
+    port_str: string | null;
     @Column({ type: 'text', nullable: true })
     text: string | null;
 }
