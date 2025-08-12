25c1f751ecb11303974c4b79cc2160eb8ad4996a
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-12 15:22:16 +0900

    진짜 이제 된다

--- src/database/sync.ts
+++ src/database/sync.ts
@@ -1,20 +1,33 @@
-import { DataSource } from 'typeorm';
+// app.module.ts
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import * as dotenv from 'dotenv';
 dotenv.config();
+
 import { TextEntity } from '../text/entities/text.entity';
 import { Port_TextEntity } from '../text/entities/port_text.entity';
-import {TypeOrmModule} from "@nestjs/typeorm";
+import { TextService } from '../text/text.service';
+import { TextController } from '../text/text.controller';
 
-TypeOrmModule.forRootAsync({
-    useFactory: () => ({
-        type: 'postgres',
-        host: process.env.DB_HOST,
-        port: parseInt(process.env.DB_PORT || '5432', 10),
-        username: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DB_DATABASE,
-        entities: [TextEntity, Port_TextEntity],
-        synchronize: true,
-        logging: true,
-    }),
-});
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            useFactory: () => ({
+                type: 'postgres',
+                host: process.env.DB_HOST,
+                port: parseInt(process.env.DB_PORT || '5432', 10),
+                username: process.env.DB_USERNAME,
+                password: process.env.DB_PASSWORD,
+                database: process.env.DB_DATABASE,
+                entities: [TextEntity, Port_TextEntity],
+                synchronize: true,
+                logging: true,
+            }),
+        }),
+
+        TypeOrmModule.forFeature([TextEntity, Port_TextEntity]),
+    ],
+    controllers: [TextController],
+    providers: [TextService],
+})
+export class AppModule {}
