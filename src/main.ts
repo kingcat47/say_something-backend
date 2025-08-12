cb205db029e62a0dbab54ddbf448f4c5db43dfac
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-13 08:09:50 +0900

    웹소켓 cors설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { IoAdapter } from '@nestjs/platform-socket.io';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.enableCors({
+    origin: ['https://saysome.thnos.app'], // 또는 개발 시 '*'
+    credentials: true,
+  });
+
+  app.useWebSocketAdapter(new IoAdapter(app));
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
