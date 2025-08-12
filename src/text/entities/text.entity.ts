2e1c417669dac79da1b229636e7e2a4147328df2
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-12 15:58:08 +0900

    엔티티에 고유아이디가 없어서 문제였노 ㅋ

--- src/text/entities/text.entity.ts
+++ src/text/entities/text.entity.ts
@@ -1,9 +1,13 @@
-import { Entity, Column } from 'typeorm';
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity('text')
 export class TextEntity {
+    @PrimaryGeneratedColumn()
+    id: number;
+
     @Column({ type: 'text', nullable: true })
     text: string | null;
+
     @Column({ type: 'int', default: 0 })
     count: number;
 }
