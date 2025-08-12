1490e784388c933c279227dd90ed3389a7e90ab1
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-12 14:28:53 +0900

    dma rmfody

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,11 +6,11 @@ import { TextModule } from './text/text.module';
   imports: [
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: 'localhost',
+      host: process.env.DB_HOST,
       port: 5432,
       username: 'postgres',  // 본인 DB 사용자명
-      password: 'password',  // 본인 DB 비밀번호
-      database: 'dbname',    // 본인 DB 이름
+      password: process.env.DB_PASSWORD,  // 본인 DB 비밀번호
+      database: 'postgres',    // 본인 DB 이름
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: true,     // 개발환경에서만 true 권장 (자동 테이블 동기화)
     }),
