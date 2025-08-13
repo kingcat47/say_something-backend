6c32a1897a16f82f7feb7a1cdb4a9cace2eabbdb
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-14 02:02:23 +0900

    아 모듈에 추가를 안해놨었네

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -4,12 +4,13 @@ import { ImageService } from './image.service';
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {ImageEntity} from "../image/entities/image.entity";
 import {Port_ImageEntity} from "../image/entities/port_image.entity";
+import {ImageGateway} from "./image.gateway";
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([ImageEntity, Port_ImageEntity]),
   ],
   controllers: [ImageController],
-  providers: [ImageService]
+  providers: [ImageService,ImageGateway]
 })
 export class ImageModule {}
