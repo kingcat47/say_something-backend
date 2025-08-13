e8254190bb2f1891a303fb14a28f599b70cf75b5
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-14 02:30:13 +0900

    사용하지 않는것들 전부지움.
    포스트그레스 연결한건 혹시 쓸지도 몰라서 냄겨둠.
    
    GIF도 할까말까 고민중

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ImageController } from './image.controller';
-import { ImageService } from './image.service';
-import {TypeOrmModule} from "@nestjs/typeorm";
-import {ImageEntity} from "../image/entities/image.entity";
-import {Port_ImageEntity} from "../image/entities/port_image.entity";
 import {ImageGateway} from "./image.gateway";
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([ImageEntity, Port_ImageEntity]),
   ],
-  controllers: [ImageController],
-  providers: [ImageService,ImageGateway]
+  providers: [ImageGateway]
 })
 export class ImageModule {}
