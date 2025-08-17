f3698e627a8f53fb9c499e5c7f9c2f530570bf2c
orangejack <79419803+kingcat47@users.noreply.github.com>
2025-08-17 16:59:49 +0900

    consturtor로 이미지에 텍스트쪽 정보 주입

--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import {ImageGateway} from "./image.gateway";
 import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
+import {TextModule} from "../text/text.module";
 
 @Module({
-  imports: [
+  imports: [TextModule
   ],
   providers: [ImageGateway, ImageService],
   controllers: [ImageController]
